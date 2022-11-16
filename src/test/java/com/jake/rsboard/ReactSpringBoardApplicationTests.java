93242c1e7d9bbb8069e9b06c4562240bd27d622b
epike <epikeran@gmail.com>
2022-11-16 16:11:20 +0900

    spring boot 환경세팅

---
+++ src/test/java/com/jake/rsboard/ReactSpringBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.rsboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
