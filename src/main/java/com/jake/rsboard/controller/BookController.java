80d2e5795500f37b5cc4d536329c18a33c322dfc
epike <epikeran@gmail.com>
2022-11-23 10:54:29 +0900

    Sprinbooot CROS 정책 해제

--- src/main/java/com/jake/rsboard/controller/BookController.java
+++ src/main/java/com/jake/rsboard/controller/BookController.java
@@ -12,27 +12,35 @@ import org.springframework.web.bind.annotation.*;
 public class BookController {
     private final BookService bookService;
 
+    // security (라이브러리 적용) - CORS 정책을 가지고 있음.(시큐리티가 CORS를 해제해야함)
+    // BookController 진입 직전
+    @CrossOrigin
     @PostMapping("/book")
     public ResponseEntity<?> save(@RequestBody Book book) {
 //        List<Book> abc = new ArrayList<>(); // 제네릭 타입 생략가능
+//        throw new IllegalArgumentException("잘못날림");
         return new ResponseEntity<>(bookService.insertBook(book), HttpStatus.CREATED); // 201
     }
 
+    @CrossOrigin
     @GetMapping("/book")
     public ResponseEntity<?> findAll() {
         return new ResponseEntity<>(bookService.selectBooks(), HttpStatus.OK); // 200
     }
 
+    @CrossOrigin
     @GetMapping("book/{id}")
     public ResponseEntity<?> findById(@PathVariable Long id) {
         return new ResponseEntity<>(bookService.selectBook(id), HttpStatus.OK); // 200
     }
 
+    @CrossOrigin
     @PutMapping("book/{id}")
     public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Book book) {
         return new ResponseEntity<>(bookService.updateBook(id, book), HttpStatus.OK); // 200
     }
 
+    @CrossOrigin
     @DeleteMapping("book/{id}")
     public ResponseEntity<?> deleteById(@PathVariable Long id) {
         return new ResponseEntity<>(bookService.deleteBook(id), HttpStatus.OK); // 200
